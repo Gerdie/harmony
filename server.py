999bab00a23638bc95d7ccd9e11ab8783380c56c
Maria <mariakmoy@gmail.com>
2017-02-11 22:56:13 +0000

    Implementing file upload

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
-from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+import os
+
+from har import allowed_file
+
+UPLOAD_FOLDER = '/uploads'
 
 app = Flask(__name__)
-
-#For debugging - see Jinja fails
-app.jinja_env.undefined = StrictUndefined
-app.jinja_env.auto_reload = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# Setting maximum allowed payload to 16 megabytes
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
 @app.route('/')
@@ -14,11 +18,27 @@ def show_home():
 
     return render_template("index.html")
 
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
 if __name__ == "__main__":
     # Change app.debug to False before launch
     app.debug = True
-
-    # Use the DebugToolbar
-    DebugToolbarExtension(app)
-
-    app.run
+    app.run(host="0.0.0.0")
